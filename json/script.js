7e30ae17a67df1af8c3b19c6cc20de9111ea02ea
Saidburxon <saidburxon@gmail.com>
2024-09-17 22:13:46 +0500

    sass

--- json/script.js
+++ json/script.js
@@ -1,67 +1,7 @@
-// function on() {
-//   document.getElementById("overlay").style.display = "block";
-// }
+// Get the loader element
+const loader = document.getElementById("loader");
 
-// function off() {
-//   document.getElementById("overlay").style.display = "none";
-// }
-
-// function myFirst() {
-//   myDisplayer("Hello");
-// }
-
-// function mySecond() {
-//   myDisplayer("Goodbye");
-// }
-
-// myFirst();
-// mySecond();
-// // Create a scanner object
-// myObj = new Scanner("A string to scan");
-
-// // Find the delimiter
-// System.out.println(myObj.delimiter());
-
-// // Close the scanner
-// myObj.close();
-// // Create a scanner object
-// myObj = new Scanner(
-//   "Please send an email to info@example.com for more details."
-// );
-// // Create a scanner object
-// myObj = new Scanner(
-//   "Please send an email to info@example.com for more details."
-// );
-
-// // Get the email address with a pattern
-// email = myObj.findWithinHorizon("[a-zA-Z]+@[a-zA-Z]+.[a-zA-Z]{2,}", 0);
-
-// // Show the email if found
-// if (email != null) {
-//   System.out.println(email);
-// } else {
-//   System.out.println("No email found");
-// }
-// // Get the email address with a pattern
-// email = myObj.findInLine("[a-zA-Z]+@[a-zA-Z]+.[a-zA-Z]{2,}");
-
-// // Show the email if found
-// if (email != null) {
-//   System.out.println(email);
-// } else {
-//   System.out.println("No email found");
-// }
-// // Create a scanner object
-// myObj = new Scanner("A string to scan");
-
-// // Read every token
-// while (myObj.hasNext()) {
-//   System.out.println(myObj.next());
-// }
-
-// // Close the scanner
-// myObj.close();
-window.addEventListener("load", function () {
-  const loader = document.querySelector(".loader");
-  loader.style.display = "none"; // Sahifa yuklanganidan keyin loaderni yashirish
-});
+// Set a timeout to hide the loader after 3 seconds
+setTimeout(() => {
+  loader.style.display = "none";
+}, 3000);
